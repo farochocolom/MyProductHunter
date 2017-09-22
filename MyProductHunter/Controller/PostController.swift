2a5d42852f10edd1ea337aeee140d147a9837361
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-22 07:07:40 -0700

    basic UI and table cell for the post created

--- MyProductHunter/Controller/PostController.swift
+++ MyProductHunter/Controller/PostController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class PostController: UIViewController {
+    
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,6 +19,7 @@ class ViewController: UIViewController {
             
             guard let postList = posts else {return}
             
+            self.posts = postList
             for post in postList {
                 print(post.name)
                 print(post.tagline)
@@ -28,7 +31,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension PostController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
 
---
+++ MyProductHunter/View/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  MyProductHunter
+//
+//  Created by Fernando on 9/22/17.
+//  Copyright © 2017 Specialist. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
